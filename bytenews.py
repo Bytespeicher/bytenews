a3f6a78fd474702d879c549bf2728386c6915f1f
Chaos <stefan@binaervarianz.de>
2016-07-24 18:19:17 +0200

    code style cleanup

--- bytenews.py
+++ bytenews.py
@@ -17,19 +17,20 @@ import pytz
 TZ = pytz.timezone('Europe/Berlin')
 
 def getStopDate():
-    ''' reads the category feed for bytespeicher notizen and gets the last publication date '''  
-    
+    ''' reads the category feed for bytespeicher notizen and gets the last publication date '''
+
     website = requests.get('https://bytespeicher.org/category/bytespeicher-notizen/feed/')
     html_source = website.text
     soup = BeautifulSoup(html_source, 'lxml-xml')
-    
-    datexml = soup.find('item').find('pubDate') 
-    
+
+    datexml = soup.find('item').find('pubDate')
+
     locale.setlocale(locale.LC_TIME, 'eng_us')
     stop = datetime.strptime(datexml.string, '%a, %d %b %Y %H:%M:%S %z')
-    
+
     return stop
-    
+
+
 def blog():
     ''' Reads feed from bytespeicher.org, extracts article titles,
     dates and links. Stops before last Bytespeicher Notizen.
@@ -61,22 +62,19 @@ def wiki(stop_date):
     dates and links. Stops before date of last Bytespeicher Notizen.
     '''
     website = requests.get("https://technikkultur-erfurt.de/start?do=recent")
-    html_source = website.text
-    soup = BeautifulSoup(html_source, 'lxml')
+    soup = BeautifulSoup(website.text, 'lxml')
 
-    forms = soup.find_all('form')
-    items = forms[1].find_all('li')
+    items = soup.find_all('form')[1].find_all('li')
     output = ''
 
     for item in items:
         output += '* '
         link = item.find_all('a', re.compile('wikilink.*'))
-        
+
         if '/freifunk:' in link[0]['href']:
-            stub = output.rfind('*')
-            output = output[:stub]
+            output = output[:output.rfind('*')]
             continue
-        
+
         output += link[0]['href']
 
         spans = item.find_all('span')
@@ -105,14 +103,15 @@ def wiki(stop_date):
         output += 'https://technikkultur-erfurt.de/' + link[0]['href'] + '\n'
 
 # get link to correct diff since last publication for reference
-        diffwebsite = requests.get("https://technikkultur-erfurt.de/" + link[0]['href'] + '?do=revisions' )
+        diffwebsite = requests.get("https://technikkultur-erfurt.de/" \
+                      + link[0]['href'] + '?do=revisions')
         diffhtml_source = diffwebsite.text
-        
+
         pyperclip.copy(diffhtml_source)
-        
+
         diffsoup = BeautifulSoup(diffhtml_source, 'lxml')
 
-        changes = diffsoup.find_all('div', 'li' )
+        changes = diffsoup.find_all('div', 'li')
         difflink = ''
         for i, c in enumerate(changes):
             date = c.span.string
@@ -120,7 +119,7 @@ def wiki(stop_date):
                 break
             if i != 0:
                 difflink = c.find('a', 'diff_link')['href']
-                        
+
         output += 'DELETEME: ' + 'https://technikkultur-erfurt.de/'+ difflink +'\n\n'
 
     return output
@@ -130,11 +129,11 @@ def redmine(stop_date):
     ''' read redmine site to get tickets modified since last notizen '''
 
     output = ""
-    
+
     website = requests.get("https://redmine.bytespeicher.org/projects/bytespeicher/issues?c%5B%5D=tracker&c%5B%5D=status&c%5B%5D=priority&c%5B%5D=subject&c%5B%5D=assigned_to&c%5B%5D=updated_on&f%5B%5D=&group_by=&set_filter=1&sort=updated_on%3Adesc%2Cid%3Adesc&utf8=%E2%9C%93")
     html_source = website.text
     soup = BeautifulSoup(html_source, 'lxml')
-    
+
     t_list = soup.find('tbody')
 
     links = [c['href'] for c in t_list.find_all('a')]
@@ -143,9 +142,9 @@ def redmine(stop_date):
     title = [c.a.string for c in t_list.find_all("td", "subject")]
     user = [c.a.string if c.a else "" for c in t_list.find_all("td", "assigned_to")]
     dates = [datetime.strptime(c.string, '%d.%m.%Y %H:%M') for c in t_list.find_all("td", "updated_on")]
-    
-    all_tickets = list(zip(links,cat, stat, title, user, dates))
-    
+
+    all_tickets = list(zip(links, cat, stat, title, user, dates))
+
     for t in all_tickets:
         output += "* " + t[1] + ': ' + t[3] + ' ' + t[2] + ' (' + t[4] + ', ' + t[5].strftime('%d %b') + ')\n'
         output += "https://redmine.bytespeicher.org" + t[0] + '\n'
@@ -153,12 +152,12 @@ def redmine(stop_date):
             stub = output.rfind('*')
             output = output[:stub]
             break
-#    pyperclip.copy(html_source)    
-    
+
     return output
 
 
 def is_not_more_tag(attr):
+    ''' helper function to filter invalid mail item'''
     if attr == '#':
         return False
     else:
@@ -167,50 +166,41 @@ def is_not_more_tag(attr):
 
 
 def mail(stop_date):
-    ''' reads the mailman archives to get active discussions since last publication '''    
+    ''' reads the mailman archives to get active discussions since last publication '''
     output = ""
-    
-    
+
     website = requests.get("https://lists.bytespeicher.org/archives/list/bytespeicher%40lists.bytespeicher.org/")
-    html_source = website.text
-    soup = BeautifulSoup(html_source, 'lxml')
-#    pyperclip.copy(html_source)
+    soup = BeautifulSoup(website.text, 'lxml')
 
     section = soup.find('section', id='most-recent')
     fields = section.find_all('a', href=is_not_more_tag)
-    import pprint
-    pp = pprint.PrettyPrinter(indent=4)
-#    pp.pprint([a if not a.h5 else '' for a in fields])
 
     links = [a['href'] for a in fields]
     topics = [a.h5 for a in fields]
     datestr = [list(a.div.stripped_strings)[0].replace('nachm.', '').replace('vorm.', '')[:-1] + " 2016" for a in fields]
-    
-    pp.pprint(datestr)    
+
     locale.setlocale(locale.LC_TIME, 'deu_deu')
 
     dates = [datetime.strptime(s, '%a %b %d, %H:%M %Y') for s in datestr]
     locale.setlocale(locale.LC_TIME, 'eng_us')
 
-#    pp.pprint([a if a else 'FAILFAILFAIL=======================================' for a in topics])
-    
     all_mail = list(zip(links, topics, dates))
-    
-    for m in all_mail:
-        output += "* " + list(m[1].stripped_strings)[1] + '(' + m[2].strftime('%d %b') + ')\n'
-        output += 'https://lists.bytespeicher.org' + m[0] + '\n'
-        if TZ.localize(m[2]) < stop_date:
+
+    for mline in all_mail:
+        output += "* " + list(mline[1].stripped_strings)[1] + '(' + mline[2].strftime('%d %b') + ')\n'
+        output += 'https://lists.bytespeicher.org' + mline[0] + '\n'
+        if TZ.localize(mline[2]) < stop_date:
             stub = output.rfind('*')
             output = output[:stub]
             break
-    return(output)
+    return output
 
 
 def main():
-        
+    ''' call all subfunctions to generate content '''
+
     stop_date = getStopDate()
 
-    ''' call all subfunctions to generate content '''
     output = '##[BLOG]\n'
     output += blog()
     output += '\n\n'
